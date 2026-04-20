b5de2d7587794b1419c5e88eec05c089785444bc
HauJul <julika.haubner@gmail.com>
2026-04-20 20:22:04 +0200

    feat: use new gschasi station

--- src/app/weather_stations/gschasi/gschasi.component.ts
+++ src/app/weather_stations/gschasi/gschasi.component.ts
@@ -8,4 +8,18 @@ import { Component } from '@angular/core';
 })
 export class GschasiComponent {
 
+    ngAfterViewInit() {
+        const existingScript = document.getElementById('openwindmap-widget-script');
+
+        if (existingScript) {
+            existingScript.remove();
+        }
+
+        const script = document.createElement('script');
+        script.id = 'openwindmap-widget-script';
+        script.src = 'https://www.openwindmap.org/js/widget-v1.js';
+        script.defer = true;
+        document.body.appendChild(script);
+    }
+
 }
